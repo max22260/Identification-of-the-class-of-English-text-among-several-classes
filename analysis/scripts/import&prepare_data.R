1bd635fbf0006f65846bd4ed56f2b13bc6143d61
ahmed nagy radwan <ahmedsurviver@gmail.com>
2018-04-20 17:37:35 +0200

    create new script to *import and prepare dataset*

---
+++ analysis/scripts/import&prepare_data.R
@@ -0,0 +1,7 @@
+# ====================================================================================================
+# title:    prepare data
+# author:   ahmed nagy radwan
+# summary: in this script import data , merge  all file with the class lable for each file on one data 
+# matrix and convert data to .csv  
+# output :  dataset.csv , datamatrix.data
+# ====================================================================================================
